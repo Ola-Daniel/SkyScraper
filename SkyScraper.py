f1582fc6ad728795fa82db6a401582549417ab04
Ola-Daniel <doola-olorun@student.oauife.edu.ng>
2022-02-16 01:33:53 +0800

    edited main function

--- SkyScraper.py
+++ SkyScraper.py
@@ -1,10 +1,14 @@
+# Python program to scrape websites and save content from website
 from bs4 import BeautifulSoup
 import html5lib
 import requests
 import csv
+import os
 
-
-URL = input('Please input a valid URL: ')
+search_id = "up"
+#    TEST_URL = "nairaland.com"
+URL = input('Please input a URL: ')
+#  inplement feature that allows script to take a file containing multiple urls or a single url
 scheme = "https://"
 if scheme in URL:
     URL = URL
@@ -12,6 +16,8 @@ else:
     URL = scheme + URL
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html5lib')
-print(soup.prettify())
+result = soup.find(id=search_id)
+print(result)
+
 
 
